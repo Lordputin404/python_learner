2d25f2be78c830bcccc1c06717b24dd68b59282a
Lordputin404 <amarkujur4583@gmail.com>
2026-05-23 12:34:00 +0000

    python: User Input

---
+++ userinput.py
@@ -0,0 +1,9 @@
+#input() it returns entered data as string
+name= input("What is your name?")
+age= input("What's your age?")
+
+age = int(age)
+age = age +1
+print(f"Hello {name}")
+print(f"Happy Birthday")
+print(f"You are {age} years old")
