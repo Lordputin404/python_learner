eec411df35f4676507df017291e21383a17c986b
Lordputin404 <amarkujur4583@gmail.com>
2026-05-23 11:39:23 +0000

    python: Conditional Expressions

---
+++ conditionalexpressions.py
@@ -0,0 +1,16 @@
+num = 4
+a = 4
+b = 6
+age = 16
+temperature = 30
+#print("Positive" if num > 0 else "Negative")
+#print("Even" if num % 2 == 0 else "Odd")
+#max_num = a if a > b else b
+#min_num = a if a < b else b
+#verify = "Adult" if age >= 18 else "Child"
+weather = "Hot" if temperature > 20 else "Cold"
+
+#print(max_num)
+#print(min_num)
+#print(verify)
+print(weather)
