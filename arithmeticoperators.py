6ecdc49bbaad5c266e385ceacef301abace80a29
Lordputin404 <amarkujur4583@gmail.com>
2026-05-23 13:13:03 +0000

    python: Arithmetic Operators

---
+++ arithmeticoperators.py
@@ -0,0 +1,17 @@
+friends = 10
+
+#friends = friends + 1
+#friends += 1
+#friends = friends - 2
+#friends -= 2
+#friends = friends * 3
+#friends *= 3
+#friends = friends / 4
+#friends /= 4
+#friends = friends ** 5
+#friends **= 5
+remainder = friends % 3
+
+
+print(f"You have {remainder} friends")
+#print(f"You have {friends} friends")
