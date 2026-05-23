b53c3dd02d256b77745c9a9413977e0146a61f71
Lordputin404 <amarkujur4583@gmail.com>
2026-05-23 13:32:43 +0000

    python: Ternary Operator

---
+++ ternaryoperator.py
@@ -0,0 +1,14 @@
+#num = int(input("Enter any number: "))
+
+#print("The number is Positive" if num > 0 else "The number is Negative")
+
+#print("The number is even" if num % 2 == 0 else "The number is odd")
+
+#a = int(input("Enter first number: "))
+#b = int(input("Enter second number: "))
+
+#print(f"The number {a} is greater than {b}" if a > b else f"The number {b} is greater than {a}")
+
+age = int(input("Enter your age:"))
+
+print("You are an adult"if age >=18 else "You are a child")
