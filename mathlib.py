2f72d8e28ea5e939670ed1a2cea123ced3bf440c
Lordputin404 <amarkujur4583@gmail.com>
2026-05-23 13:15:13 +0000

    python: Math Lib

---
+++ mathlib.py
@@ -0,0 +1,11 @@
+import math
+
+x = 9
+
+#print(math.pi)
+#print(math.e)
+
+#result = math.sqrt(x) #squareroot
+#result = math.ceil(x) #roundsup
+result = math.floor(x) #roundsdown
+print(int(result))
