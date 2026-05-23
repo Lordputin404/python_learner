2889d88a8d3ea5fa9dc0be4ae8088fd2d7243594
Lordputin404 <amarkujur4583@gmail.com>
2026-05-23 13:16:04 +0000

    python: If Statements

---
+++ ifstatements.py
@@ -0,0 +1,13 @@
+age = int(input("Enter your age:"))
+
+if age>100 :
+    print("You are very old enough")
+
+elif age >= 18:
+    print("You are an adult")
+
+elif age<0:
+    print("You are not even born yet")
+
+else :
+    print("You are a child")  
