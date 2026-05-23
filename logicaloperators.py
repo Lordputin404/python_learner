b9d1eedbedc4d306ffab5f43eb759e51735de87a
Lordputin404 <amarkujur4583@gmail.com>
2026-05-23 13:28:42 +0000

    python: Logical Operators

---
+++ logicaloperators.py
@@ -0,0 +1,21 @@
+#and
+#or
+#not
+
+#temp = -5
+#is_raining = False
+
+#if temp > 35 or temp < 0 or is_raining :
+#   print("The trip plan is cancelled")
+
+#else :
+#   print("The trip is still scheduled")
+
+temp = 25
+is_friendsavailable = True
+
+if temp < 30 and is_friendsavailable :
+    print("The trip will be held")
+
+else : 
+    print("The trip is cancelled")
