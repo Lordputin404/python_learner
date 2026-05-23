74f72b851022671653027e3856ed43c675067adb
Lordputin404 <amarkujur4583@gmail.com>
2026-05-23 13:09:42 +0000

    python: Type Casting

---
+++ typecasting.py
@@ -0,0 +1,21 @@
+# str(), int(), float(), bool()
+name = "Amar"
+age = 18
+cgpa = 7.5
+is_student = True
+
+#check
+print(type(name))
+print(type(age))
+print(type(cgpa))
+print(type(is_student))
+
+#float to int, int to float
+cgpa = int(cgpa)
+print(cgpa)
+age = float(age)
+print(age)
+
+#int to str
+age = str(age)
+print(type(age))
